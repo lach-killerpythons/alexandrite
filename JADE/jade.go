cafe2c82b20e8e6ae7b2876e212e53c7b1c97b82
Lachlan <lachlanmcbride5@gmail.com>
2025-07-12 12:15:37 +1000

    updates

--- JADE/jade.go
+++ JADE/jade.go
@@ -1,5 +1,3 @@
-//package main
-
 package JADE
 
 import (
--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	_ "github.com/lach-killerpythons/alexandrite/BLUE" // Import the BLUE package
-	_ "github.com/lach-killerpythons/alexandrite/JADE" // Import the JADE package
-	_ "github.com/lach-killerpythons/alexandrite/RED"  // Import the RED package
+	//_ "github.com/lach-killerpythons/alexandrite/BLUE" // Import the BLUE package
+	//_ "github.com/lach-killerpythons/alexandrite/JADE" // Import the JADE package
+	//_ "github.com/lach-killerpythons/alexandrite/RED"  // Import the RED package
 )
 
 func main() {
