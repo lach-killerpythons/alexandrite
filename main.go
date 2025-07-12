d8d9be74089f09bef29f00f3fd1fe4517802fe0c
Lachlan <lachlanmcbride5@gmail.com>
2025-07-12 12:32:17 +1000

    fixed folders'

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	//_ "github.com/lach-killerpythons/alexandrite/BLUE" // Import the BLUE package
+
+	_ "github.com/lach-killerpythons/alexandrite/BLUE" // Import the BLUE package
+
 	//_ "github.com/lach-killerpythons/alexandrite/JADE" // Import the JADE package
-	//_ "github.com/lach-killerpythons/alexandrite/RED"  // Import the RED package
+	_ "github.com/lach-killerpythons/alexandrite/RED" // Import the RED package
 )
 
 func main() {
